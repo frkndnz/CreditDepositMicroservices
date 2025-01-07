ab6aada65f2ef5f35a2ddcb374469038bbe7716a
Furkan Deniz <dnzfurkan@outlook.com>
2025-01-07 13:37:47 +0300

    UpdateController

--- CustomerService/Controllers/CustomerController.cs
+++ CustomerService/Controllers/CustomerController.cs
@@ -22,11 +22,31 @@ namespace CustomerService.Controllers
            return customers.Any() ? Ok(customers) : NotFound("customers not found!");
             
         }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomersById(int id)
+        {
+            var customer = await _customerService.GetCustomerAsync(id);
+            return customer != null ? Ok(customer) : NotFound("customer not found!");
+        }
 
         [HttpPost]
         public async Task<IActionResult> AddCustomer(CreateCustomerDTO createCustomerDTO)
         {
             var result = await _customerService.AddCustomerAsync(createCustomerDTO);
+            return result.IsSuccess ? Ok(result.Message) : BadRequest(result.Message);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCustomer(CreateCustomerDTO createCustomerDTO)
+        {
+            var result = await _customerService.UpdateCustomerAsync(createCustomerDTO);
+            return result.IsSuccess ? Ok(result.Message) : BadRequest(result.Message);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var result = await _customerService.DeleteCustomerAsync(id);
             return result.IsSuccess ? Ok(result.Message) : BadRequest(result.Message);
         }
     }
